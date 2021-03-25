9b47eb5c30ff347c8d6a19170ef27b55bd8951ae
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-25 23:03:28 +0200

    used subscribe method to get data #8

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../ mock-heroes';
+import { HeroService } from '../services/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +11,15 @@ import { HEROES } from '../ mock-heroes';
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
-  heroes: Hero[] = HEROES;
+  heroes: Hero[] = [];
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.heroService.getHeroes().subscribe((response) => {
+      this.heroes = response;
+    });
+  }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
