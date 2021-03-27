babc6e285c06b0f242207d9179a4104f457264e4
ranasobeid95 <ranasobeid95@gmail.com>
2021-03-27 18:04:28 +0300

    inject HttpClient into the constructor in a private property called http Define the heroesUrl Get heroes with HttpClient Error handling Update the HeroService getHero() to get it from API #10

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,21 +3,45 @@ import { Hero } from '../hero';
 import { HEROES } from '../ mock-heroes';
 import { of, Observable } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { error } from 'console';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('Fetched heros')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed : ${error.message}`);
+      return of(result as T);
+    };
+  }
+
   getHero(id: number): Observable<Hero> {
-    const hero = of(HEROES.find((ele) => ele.id === id) as Hero);
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return hero;
+    const url = `this.heroesUrl/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`Fetched hero id ${id}`)),
+      catchError(this.handleError<Hero>(`Failed fetched hero id ${id}`))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`Hero Service : ${message}`);
   }
 }
